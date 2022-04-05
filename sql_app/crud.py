07e9037c629a086e24f7936756edc410dee3adfc
Renata Yun <55555221+Renata-Yun@users.noreply.github.com>
2022-04-05 21:55:47 +0300

    test1 commit

---
+++ sql_app/crud.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 200):
+    return db.query(models.User).offset(skip).limit(limit).all()
