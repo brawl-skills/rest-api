78e44f54eb2bb36c010e83ec88ff41cefd649a33
Germain <adedihagermain@gmail.com>
2022-04-17 00:43:34 +0300

    credentials changed

--- db_locally/database.py
+++ db_locally/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import  create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://germain:17mawuko17@localhost/my_db"
+db_url = "postgresql://brawl:123456@localhost/brawl_skills_db"
 
 engine = create_engine(db_url)
 
