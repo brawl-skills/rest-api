f858c0bfca0703e18e85a5a62599ea66af8bcfdf
Germain <adedihagermain@gmail.com>
2022-04-30 01:45:42 +0300

    getting all updates

--- db_locally/crud.py
+++ db_locally/crud.py
@@ -4,3 +4,6 @@ from . import models
 
 def get_player_trophies(db:Session,player_id:int):
     return db.query(models.BattlelogPlayers).filter(models.BattlelogPlayers.id == player_id).first()
+
+def get_all_last_updates(db:Session):
+    return db.query(models.Players).all()
